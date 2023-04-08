93251ecef5baaee04c32ae68a05d841368320fca
Mina329 <minaemil329@gmail.com>
2023-04-08 02:18:35 +0200

    Add: background of SettingPage

--- lib/src/features/settings/view/pages/settings_page.dart
+++ lib/src/features/settings/view/pages/settings_page.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:settings_ui/settings_ui.dart';
 
+import '../../../../konstants/kcolors.dart';
+
 class SettingsPage extends ConsumerWidget {
   const SettingsPage({super.key});
 
@@ -20,82 +22,101 @@ class SettingsPage extends ConsumerWidget {
         icon: Icons.settings,
       ),
       drawer: const AppDrawerWidget(),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text('appearance'),
-            tiles: [
-              SettingsTile.switchTile(
-                onToggle: (_) {
-                  ref
-                      .read(settingsControllerProvider.notifier)
-                      .toggleThemeMode();
-                },
-                initialValue: settingsState.mode == ThemeMode.light,
-                leading: const Icon(Icons.sunny_snowing),
-                title: const Text('mode'),
-                trailing: Icon(settingsState.mode.icon),
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.theater_comedy_outlined),
-                title: const Text('color scheme'),
-                trailing: DropdownButton(
-                  icon: const Icon(Icons.arrow_downward_rounded),
-                  value: settingsState.scheme,
-                  items: [
-                    for (var scheme in FlexScheme.values)
-                      DropdownMenuItem(
-                        value: scheme,
-                        child: Text(scheme.name),
-                        onTap: () {
-                          ref
-                              .read(settingsControllerProvider.notifier)
-                              .changeThemeScheme(scheme);
-                        },
-                      ),
-                  ],
-                  onChanged: (_) {},
-                ),
-              ),
-              SettingsTile.switchTile(
-                initialValue: settingsState.useMaterial3,
-                onToggle: (_) {
-                  ref
-                      .read(settingsControllerProvider.notifier)
-                      .toggleMaterial3();
-                },
-                leading: const Icon(Icons.interests_rounded),
-                title: const Text('Enable Material 3'),
-              )
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              KColors.gradiant1,
+              KColors.gradiant2,
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          SettingsSection(
-            title: const Text('Localization'),
-            tiles: [
-              SettingsTile.navigation(
-                leading: const Icon(Icons.language),
-                title: const Text('Language'),
-                trailing: DropdownButton(
-                  icon: const Icon(Icons.arrow_downward_rounded),
-                  value: settingsState.locale,
-                  items: [
-                    for (var locale in KLocales.supported)
-                      DropdownMenuItem(
-                        value: locale,
-                        child: Text(locale.toLanguageTag()),
-                        onTap: () {
-                          ref
-                              .read(settingsControllerProvider.notifier)
-                              .changeLocale(locale);
-                        },
+        ),
+        child: Container(
+          color: KColors.dark50Opacity,
+          child: Container(
+            color: Colors.transparent,
+            //TODO: setting list has default color for the background that cannot be changed
+            child: SettingsList(
+              sections: [
+                SettingsSection(
+                  title: const Text('appearance'),
+                  tiles: [
+                    SettingsTile.switchTile(
+                      onToggle: (_) {
+                        ref
+                            .read(settingsControllerProvider.notifier)
+                            .toggleThemeMode();
+                      },
+                      initialValue: settingsState.mode == ThemeMode.light,
+                      leading: const Icon(Icons.sunny_snowing),
+                      title: const Text('mode'),
+                      trailing: Icon(settingsState.mode.icon),
+                    ),
+                    SettingsTile.navigation(
+                      leading: const Icon(Icons.theater_comedy_outlined),
+                      title: const Text('color scheme'),
+                      trailing: DropdownButton(
+                        icon: const Icon(Icons.arrow_downward_rounded),
+                        value: settingsState.scheme,
+                        items: [
+                          for (var scheme in FlexScheme.values)
+                            DropdownMenuItem(
+                              value: scheme,
+                              child: Text(scheme.name),
+                              onTap: () {
+                                ref
+                                    .read(settingsControllerProvider.notifier)
+                                    .changeThemeScheme(scheme);
+                              },
+                            ),
+                        ],
+                        onChanged: (_) {},
                       ),
+                    ),
+                    SettingsTile.switchTile(
+                      initialValue: settingsState.useMaterial3,
+                      onToggle: (_) {
+                        ref
+                            .read(settingsControllerProvider.notifier)
+                            .toggleMaterial3();
+                      },
+                      leading: const Icon(Icons.interests_rounded),
+                      title: const Text('Enable Material 3'),
+                    )
                   ],
-                  onChanged: (_) {},
                 ),
-              ),
-            ],
-          )
-        ],
+                SettingsSection(
+                  title: const Text('Localization'),
+                  tiles: [
+                    SettingsTile.navigation(
+                      leading: const Icon(Icons.language),
+                      title: const Text('Language'),
+                      trailing: DropdownButton(
+                        icon: const Icon(Icons.arrow_downward_rounded),
+                        value: settingsState.locale,
+                        items: [
+                          for (var locale in KLocales.supported)
+                            DropdownMenuItem(
+                              value: locale,
+                              child: Text(locale.toLanguageTag()),
+                              onTap: () {
+                                ref
+                                    .read(settingsControllerProvider.notifier)
+                                    .changeLocale(locale);
+                              },
+                            ),
+                        ],
+                        onChanged: (_) {},
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
